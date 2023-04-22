97f5572f51b50239446e7dd308490956bd3069b6
Ali Eray <alieraay@gmail.com>
2023-04-22 18:04:54 +0300

    lottery contract completed

--- backend/contracts/Lottery.sol
+++ backend/contracts/Lottery.sol
@@ -9,15 +9,30 @@
 // Chainlil Oracle --> Randomness, Automated Execution
 
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+
+
 pragma solidity ^0.8.18;
 
 error EnterLottery__NotEnoughEntryAmount();
 error EnterLottery__AlreadyParticipated();
+error Lottery__TransferFailed();
+error Lottery__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers);
 
-contract Raffle {
+/** @title A Sample Lottery Contract
+ * @author Ali Eray
+ * @notice This contract is for creating decentralized lottery smart contract
+ * @dev This implements Chainlik VRF v2 and Chainlik Keepers
+ */
 
-    mapping(uint256 => mapping(address => bool)) public lotteryIdToCandidates;
-    mapping(uint256 => mapping(uint256 => address)) public lotteryToTicketIdToAddress;
+// TODO: this contract shouldn't be abstract but it gives error untill completed. Don't forget to fix
+contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
+
+    mapping(uint256 => mapping(address  => bool)) public lotteryIdToCandidates;
+    mapping(uint256 => mapping(uint256 => address )) public lotteryToTicketIdToAddress;
 
     // State variables
 
@@ -25,12 +40,40 @@ contract Raffle {
     uint256 private lotteryId;
     uint256 private bonusAmount;
     uint256 private ticketIdCounter;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_keyHash;
+    uint64 private immutable i_subscriptionId;
+    uint16 private immutable i_requestConfirmation;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private immutable i_numWords;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
+    
 
     // Events
     event LotteryEnter(address indexed candidate );
+    event IdRequest(uint256 indexed requestId);
+    event WinnerSelected(address indexed winner);
 
-    constructor(uint256 entryAmount) {
+    constructor(
+        address vrfCoordinatorV2, 
+        uint256 entryAmount, 
+        bytes32 keyHash, 
+        uint64 subscriptionId,
+        uint16 requestConfirmation,
+        uint32 callbackGasLimit,
+        uint32 numWords,
+        uint256 interval
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entryAmount = entryAmount;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_keyHash = keyHash;
+        i_subscriptionId = subscriptionId;
+        i_requestConfirmation = requestConfirmation;
+        i_callbackGasLimit = callbackGasLimit;
+        i_numWords = numWords;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
     function enterLottery() public payable {
@@ -47,12 +90,62 @@ contract Raffle {
         emit LotteryEnter(msg.sender);
     }
 
-    function pickRandomWinner() public  returns (address) {
+    /**
+     * @dev Chainlink Keeper
+     * Some requirements needed
+     * 1. correct time
+     * 2. at least 1 person
+     * 3. subscription fund with Link
+     * 4. lottert should be open
+     */
+ 
+    function checkUpkeep(
+        bytes memory  /* checkData */
+    ) 
+        public 
+        override 
+        returns (
+            bool upkeepNeeded,
+            bytes memory performData 
+            ) 
+        {
+        bool timePassed = (block.timestamp - s_lastTimeStamp) > i_interval;
+        bool hasPlayers = (ticketIdCounter > 0);
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (timePassed && hasPlayers && hasBalance);
+    }
+
+    function performUpkeep(bytes calldata /* performData */ ) external override {
         // We will just get a random number which will be winner tickedId
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if(!upkeepNeeded) {
+            revert Lottery__UpkeepNotNeeded(address(this).balance, ticketIdCounter);
+        }
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_keyHash,
+            i_subscriptionId,
+            i_requestConfirmation,
+            i_callbackGasLimit,
+            i_numWords
+        );
+        emit IdRequest(requestId);
+    }
+
+    function fulfillRandomWords(uint256 /* requestId */, uint256[] memory randomWords) internal override  {
+
+        uint256 winnerTicketId = randomWords[0] % (ticketIdCounter+1);
+        ticketIdCounter = 0;
+        address recentWinner = lotteryToTicketIdToAddress[lotteryId][winnerTicketId];
+        (bool success,) = recentWinner.call{value: address(this).balance}("");
+        if(!success){
+            revert Lottery__TransferFailed();
+        } 
+        lotteryId++;
+        s_lastTimeStamp = block.timestamp;
+        emit WinnerSelected(recentWinner);
     }
 
     function payToWinner() public {
-        lotteryId++;
     }
     function getEntryAmount() public view returns(uint256){
         return i_entryAmount;
@@ -62,5 +155,11 @@ contract Raffle {
     }
     function getPlayer(uint256 _lotteryId, uint256 _ticketId) public view returns(address){
         return lotteryToTicketIdToAddress[_lotteryId][_ticketId];
+    }
+    function getNumberOfPlayer() public view returns(uint256) {
+        return ticketIdCounter;
+    }
+    function getLatestTimeStamp() public view returns(uint256) {
+        return block.timestamp;
     }
 }
