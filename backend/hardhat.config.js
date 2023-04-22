19c62560a83317cadcd676070e30ffb577f48a0e
Ali Eray <alieraay@gmail.com>
2023-04-22 18:07:22 +0300

    update

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -4,14 +4,37 @@ require("hardhat-deploy")
 require("solidity-coverage")
 require("hardhat-contract-sizer")
 require("hardhat-gas-reporter")
-require("dotenv").config()
+require("dotenv").config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.18",
 
-  aa: " 123",
-      11: "1233",
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const GOERLI_URL = process.env.GOERLI_URL
+const COIN_MARKET_CAP_API_KEY = process.env.COIN_MARKET_CAP_API_KEY
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {version: "0.8.18"},
+    ],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_URL,
+      chainId: 5,
+      accounts : [PRIVATE_KEY],
+      blockConfirmations: 6,
+    },
+    
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
