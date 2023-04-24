833d5ceee1344e752ee275d36970e81633bffd79
Ali Eray <alieraay@gmail.com>
2023-04-24 04:58:00 +0300

    updated

--- backend/helper-hardhat-config.js
+++ backend/helper-hardhat-config.js
@@ -8,20 +8,20 @@ const networkConfig = {
         keyHash: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         subscriptionId: "11366",
         requestConfirmation: "3",
-        callbackGasLimit: "500000",
+        callbackGasLimit: "5000000",
         numWords: "1",
-        interval: "60"
+        interval: "180"
     },
     31337: {
         name: "localhost",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
         entryPrice: ethers.utils.parseEther("0.01"),
         keyHash: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-        subscriptionId: "11366",
-        requestConfirmation: "3",
-        callbackGasLimit: "500000",
+        subscriptionId: "588",
+        requestConfirmation: "1",
+        callbackGasLimit: "5000000",
         numWords: "1",
-        interval: "60"
+        interval: "30"
     },
 
 }
