757aa5d765c88350d3def9b2ec533d0afb590979
Ali Eray <alieraay@gmail.com>
2023-05-11 05:19:58 +0300

    added feature to get ticketId's msg.sender

--- backend/contracts/Lottery.sol
+++ backend/contracts/Lottery.sol
@@ -14,7 +14,6 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
-
 pragma solidity ^0.8.18;
 
 error EnterLottery__NotEnoughEntryPrice();
@@ -31,9 +30,9 @@ error Lottery__IsNotActive();
  */
 
 contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
-
-    mapping(uint256 => mapping(address  => bool)) public lotteryIdToCandidates;
-    mapping(uint256 => mapping(uint256 => address )) public lotteryToTicketIdToAddress;
+    mapping(uint256 => mapping(address => bool)) public lotteryIdToCandidates;
+    mapping(uint256 => mapping(uint256 => address)) public lotteryToTicketIdToAddress;
+    mapping(address => mapping(uint => uint)) addressTolotteryIdToTicketId;
 
     // State variables
 
@@ -52,17 +51,16 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
     address private immutable i_owner;
     address recentWinner;
     bool private isActive = true;
-    
 
     // Events
-    event LotteryEnter(address indexed candidate );
+    event LotteryEnter(address indexed candidate);
     event IdRequest(uint256 indexed requestId);
     event WinnerSelected(address indexed winner);
 
     constructor(
-        address vrfCoordinatorV2, 
-        uint256 entryPrice, 
-        bytes32 keyHash, 
+        address vrfCoordinatorV2,
+        uint256 entryPrice,
+        bytes32 keyHash,
         uint64 subscriptionId,
         uint16 requestConfirmation,
         uint32 callbackGasLimit,
@@ -82,24 +80,25 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
     }
 
     function godMode() public {
-        if(i_owner != msg.sender){
+        if (i_owner != msg.sender) {
             revert GodMode__OnlyOwner();
         }
         isActive = !isActive;
     }
 
     function enterLottery() public payable {
-        if(!isActive){
+        if (!isActive) {
             revert Lottery__IsNotActive();
         }
-        if(lotteryIdToCandidates[lotteryId][msg.sender] == true){
+        if (lotteryIdToCandidates[lotteryId][msg.sender] == true) {
             revert EnterLottery__AlreadyParticipated();
         }
-        if(msg.value != i_entryPrice){
+        if (msg.value != i_entryPrice) {
             revert EnterLottery__NotEnoughEntryPrice();
         }
         lotteryIdToCandidates[lotteryId][msg.sender] = true;
         lotteryToTicketIdToAddress[lotteryId][ticketIdCounter] = msg.sender;
+        addressTolotteryIdToTicketId[msg.sender][lotteryId] = ticketIdCounter;
         bonusAmount += msg.value;
         ticketIdCounter++;
         emit LotteryEnter(msg.sender);
@@ -113,30 +112,23 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
      * 3. subscription fund with Link
      * 4. lottert should be open
      */
- 
+
     function checkUpkeep(
-        bytes memory  /* checkData */) 
-        public 
-        override 
-        returns (
-            bool upkeepNeeded,
-            bytes memory performData 
-            ) 
-        {
-            
+        bytes memory /* checkData */
+    ) public override returns (bool upkeepNeeded, bytes memory performData) {
         bool timePassed = (block.timestamp - s_lastTimeStamp) > i_interval;
         bool hasPlayers = (ticketIdCounter > 0);
         bool hasBalance = address(this).balance > 0;
         upkeepNeeded = (timePassed && hasPlayers && hasBalance && isActive);
     }
 
-    function performUpkeep(bytes calldata /* performData */ ) external override {
-        if(!isActive){
+    function performUpkeep(bytes calldata /* performData */) external override {
+        if (!isActive) {
             revert Lottery__IsNotActive();
         }
         // We will just get a random number which will be winner tickedId
         (bool upkeepNeeded, ) = checkUpkeep("");
-        if(!upkeepNeeded) {
+        if (!upkeepNeeded) {
             revert Lottery__UpkeepNotNeeded(address(this).balance, ticketIdCounter);
         }
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
@@ -149,52 +141,64 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
         emit IdRequest(requestId);
     }
 
-    function fulfillRandomWords(uint256 requestId , uint256[] memory randomWords) internal override  {
-        if(!isActive){
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        if (!isActive) {
             revert Lottery__IsNotActive();
         }
         uint256 winnerTicketId = randomWords[0] % (ticketIdCounter);
-        
+
         recentWinner = lotteryToTicketIdToAddress[lotteryId][winnerTicketId];
-        (bool success,) = recentWinner.call{value: address(this).balance}("");
-        if(!success){
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
             revert Lottery__TransferFailed();
         }
-        ticketIdCounter = 0; 
+        ticketIdCounter = 0;
         lotteryId++;
         s_lastTimeStamp = block.timestamp;
         emit WinnerSelected(recentWinner);
     }
+
     function getMoneyOnlyOwner() public {
-        if(i_owner != msg.sender){
+        if (i_owner != msg.sender) {
             revert GodMode__OnlyOwner();
         }
-        (bool sent,) = i_owner.call{value: address(this).balance}("");
+        (bool sent, ) = i_owner.call{value: address(this).balance}("");
         require(sent, "Failed to send Ether");
     }
-    function getEntryPrice() public view returns(uint256){
+
+    function getEntryPrice() public view returns (uint256) {
         return i_entryPrice;
     }
-    function getLotteryId() public view returns(uint256){
+
+    function getLotteryId() public view returns (uint256) {
         return lotteryId;
     }
-    function getPlayer(uint256 _lotteryId, uint256 _ticketId) public view returns(address){
+
+    function getPlayer(uint256 _lotteryId, uint256 _ticketId) public view returns (address) {
         return lotteryToTicketIdToAddress[_lotteryId][_ticketId];
     }
-    function getNumberOfPlayer() public view returns(uint256) {
+
+    function getNumberOfPlayer() public view returns (uint256) {
         return ticketIdCounter;
     }
-    function getLatestTimeStamp() public view returns(uint256) {
+
+    function getLatestTimeStamp() public view returns (uint256) {
         return block.timestamp;
     }
-    function getInterval() public view returns(uint256) {
+
+    function getInterval() public view returns (uint256) {
         return i_interval;
     }
-    function isContractActive() public view returns(bool) {
+
+    function isContractActive() public view returns (bool) {
         return isActive;
     }
-    
-    function getRecentWinner() public view returns(address) {
+
+    function getRecentWinner() public view returns (address) {
         return recentWinner;
+    }
+
+    function getYourId() public view returns (uint) {
+        return addressTolotteryIdToTicketId[msg.sender][lotteryId];
     }
 }
