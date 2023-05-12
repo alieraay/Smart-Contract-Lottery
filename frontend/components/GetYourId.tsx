6cf514fee0ff21e2e68ee82a79f35a4c6fb20a36
Ali Eray <alieraay@gmail.com>
2023-05-12 04:45:44 +0300

    button updated

--- frontend/components/GetYourId.tsx
+++ frontend/components/GetYourId.tsx
@@ -10,10 +10,11 @@ interface contractAddressesInterface {
 
 function GetYourId() {
     const addresses: contractAddressesInterface = contractAddresses
-    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const { chainId: chainIdHex, isWeb3Enabled, account } = useMoralis()
     const chainId: string = parseInt(chainIdHex!).toString()
     const lotteryAddress = chainId in addresses ? addresses[chainId][0] : null
     const [yourId, setYourId] = useState("0")
+    const [buttonClicked, setButtonClicked] = useState(false)
     const { runContractFunction: getYourId } = useWeb3Contract({
         abi: abi,
         contractAddress: lotteryAddress!,
@@ -27,22 +28,37 @@ function GetYourId() {
         if (id !== undefined) {
             const yourIdFromCall = ((await getYourId()) as BigNumber).toString()
             setYourId(yourIdFromCall)
-        } else{
+        } else {
             setYourId("undefined")
         }
     }
     useEffect(() => {
         if (isWeb3Enabled) {
-            getYourIdFromContract()
+            
         }
     }, [isWeb3Enabled])
+
+    useEffect(() => {
+        setButtonClicked(false)
+    }, [account])
+
+    const handleButtonClick = () => {
+        getYourIdFromContract()
+        setButtonClicked(true)
+    }
     return (
         <div>
-            { yourId!="undefined" ? 
-            <div> Your ticket id is {yourId}</div> 
-            : <div> You have not entered the lotteryhh</div> 
-                
-            }
+            {lotteryAddress ? (
+                <>
+                    <button onClick={handleButtonClick}>Get Your Ticket Id</button>
+
+                    {buttonClicked ? (yourId != "undefined" ? (
+                        <div> Your ticket id is {yourId}</div>
+                    ) : (
+                        <div> You have not entered the lottery</div>
+                    )): null}
+                </>
+            ) : null}
         </div>
     )
 }
