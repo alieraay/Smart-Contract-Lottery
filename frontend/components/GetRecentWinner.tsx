38caf43b513f99367557a1f1469de1de1980fc58
Ali Eray <alieraay@gmail.com>
2023-05-19 23:04:36 +0300

    listening event fixed

--- frontend/components/GetRecentWinner.tsx
+++ frontend/components/GetRecentWinner.tsx
@@ -3,6 +3,7 @@ import { MoralisProvider, useMoralis, useWeb3Contract } from "react-moralis"
 import { BigNumber, ethers, ContractTransaction, Contract } from "ethers"
 import { useEffect, useState } from "react"
 import { useContractAddress } from "@/hooks/useContractAddress"
+import { resolve } from "path"
 
 interface contractAddressesInterface {
     [key: string]: string[]
@@ -12,6 +13,7 @@ function GetRecentWinner() {
     const { isWeb3Enabled, web3 } = useMoralis()
     const lotteryAddress = useContractAddress()
     const [recentWinner, setRecentWinner] = useState("0")
+    const [updatedLotteryId, setLotteryId] = useState("0")
 
     const { runContractFunction: getRecentWinner } = useWeb3Contract({
         contractAddress: lotteryAddress!,
@@ -19,34 +21,50 @@ function GetRecentWinner() {
         functionName: "getRecentWinner",
         params: {},
     })
+
+    const {runContractFunction: getLotteryId} = useWeb3Contract({
+        contractAddress: lotteryAddress!,
+        abi: abi,
+        functionName: "getLotteryId",
+        params: {},
+
+    })
+
     async function getRecentWinnerFromContract() {
         const winner = await getRecentWinner()
+        const getLotteryIdFromContract = (await getLotteryId() as BigNumber).toString()
 
         if (winner !== null) {
-            const winnerFromCall = winner as string
+            const winnerFromCall = (winner as string)
             setRecentWinner(winnerFromCall)
-            console.log("GetWinner: ", recentWinner)
+            setLotteryId(getLotteryIdFromContract)
+            console.log("WINNER FROM cont.:", winnerFromCall)
+            console.log("LOTTERY ID FROM cont.:", winnerFromCall)
+
         } else {
             console.log("The winner has not been selected yet")
         }
     }
 
-    const checkEvents = async function () {
-        const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/")
-        let a = new ethers.Contract(lotteryAddress!, abi as any, provider)
-        a.on("WinnerSelected", (recentWinner) => {
-            getRecentWinnerFromContract()
-        })
-    }
-
     useEffect(() => {
-        if (isWeb3Enabled) {
-            getRecentWinnerFromContract()
-            checkEvents()
+        if (!isWeb3Enabled && !lotteryAddress) return
+        const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/")
+        let contract = new ethers.Contract(lotteryAddress!, abi as any, provider)
+        getRecentWinnerFromContract()
+        const listener = (winner: string,lotteryId:BigNumber,ticketId:BigNumber ) => {
+            console.log(winner)
+            setRecentWinner(winner)
+            console.log(lotteryId)
+            setLotteryId(lotteryId.toString())
         }
+        contract.on("WinnerSelected", listener)
+        return () => {
+            contract.off("WinnerSelected", listener)
+        }
+        
     }, [isWeb3Enabled])
 
-    return <div>Recent Winner {recentWinner}</div>
+    return <div>Recent Winner {recentWinner} Active Lottery Id: {updatedLotteryId}</div>
 }
 
-export default GetRecentWinner
+export default GetRecentWinner 
