a26dd312d49f3d960d423678d0f967f326a5ca31
Ali Eray <alieraay@gmail.com>
2023-05-23 00:57:41 +0300

    new feature: animation

--- frontend/components/GodModeButton.tsx
+++ frontend/components/GodModeButton.tsx
@@ -10,73 +10,116 @@ import { useNotification } from "web3uikit"
 import { handleErrorMessage } from "../utils/errorUtils"
 import { useContractAddress } from "@/hooks/useContractAddress"
 
-interface contractAddressesInterface {
-    [key: string]: string[]
-}
-function GodModeButton() {
-    const { isWeb3Enabled } = useMoralis()
-    const lotteryAddress = useContractAddress()
-    const { numPlayer } = useLotteryState()
-
-    const dispatch = useNotification()
-
-    const { runContractFunction: godMode } = useWeb3Contract({
-        abi: abi,
-        contractAddress: lotteryAddress!,
-        functionName: "godMode",
-        params: {},
-    })
-
-    const handleSuccess = async function (tx: ContractTransaction) {
-        await tx.wait(1)
-        handleNewNotification()
-    }
-
-    const handleNewNotification = function () {
-        dispatch({
-            type: "info",
-            message: "Transaction Complete!",
-            title: "Tx Notification",
-            position: "topR",
-        })
-    }
-
-    const handleError = async function (error: any) {
-        console.log("handling error", error)
-        const message = handleErrorMessage(error, "errGod")
-        handleNewErrorNotification(message)
-    }
-
-    const handleNewErrorNotification = function (message: string) {
-        dispatch({
-            type: "error",
-            title: "Transaction Reverted",
-            message: message,
-            position: "topR",
-        })
-    }
-
-    return (
-        <div>
-            {lotteryAddress ? (
-                <div className="flex flex-col items-center text-[20px] bg-white">
-                    <button
-                        onClick={async function () {
-                            await godMode({
-                                onSuccess: (tx) => handleSuccess(tx as ContractTransaction),
-                                onError: (error: any) => handleError(error),
-                            })
-                        }}
-                    >
-                        {" "}
-                        GodMode
-                    </button>
-                </div>
-            ) : (
-                <div></div>
-            )}
+function FunnyImageModal({ isOpen, closeModal }: { isOpen: boolean, closeModal: () => void }) {
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-black bg-opacity-50 absolute inset-0"></div>
+          <div className="bg-white p-5 rounded-md funny-image-container">
+            <img src="/godMeme.webp" alt="Error Image" className="error-image" />
+            <button
+              className="text-white bg-red-500 px-3 py-1 rounded-md mt-3"
+              onClick={closeModal}
+            >
+              Kapat
+            </button>
+          </div>
         </div>
-    )
+      )}
+
+      <style jsx>{`
+        .funny-image-container {
+          animation: explode 1s forwards;
+        }
+
+        .error-image {
+          opacity: 100;
+        }
+
+        @keyframes explode {
+          0% {
+            transform: scale(10);
+          }
+          100% {
+            transform: scale(1);
+          }
+        }
+      `}</style>
+    </>
+  );
 }
 
-export default GodModeButton
+function GodModeButton() {
+  const [isOpen, setIsOpen] = useState(false);
+  const lotteryAddress = useContractAddress();
+  const { numPlayer } = useLotteryState();
+
+  const dispatch = useNotification();
+
+  const { runContractFunction: godMode } = useWeb3Contract({
+    abi: abi,
+    contractAddress: lotteryAddress!,
+    functionName: "godMode",
+    params: {},
+  });
+
+  const handleSuccess = async function (tx: ContractTransaction) {
+    await tx.wait(1);
+    handleNewNotification();
+  };
+
+  const handleNewNotification = function () {
+    dispatch({
+      type: "info",
+      message: "Transaction Complete!",
+      title: "Tx Notification",
+      position: "topR",
+    });
+  };
+
+  const handleError = async function (error: any) {
+    console.log("handling error", error);
+    const message = handleErrorMessage(error, "errGod");
+    handleNewErrorNotification(message);
+    setIsOpen(true);
+  };
+
+  const handleNewErrorNotification = function (message: string) {
+    dispatch({
+      type: "error",
+      title: "Transaction Reverted",
+      message: message,
+      position: "topR",
+    });
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      {lotteryAddress ? (
+        <div className="btn-shape  p-1 flex flex-col items-center text-[20px] ml-10 bg-red-800">
+          <button
+            onClick={async function () {
+              await godMode({
+                onSuccess: (tx) => handleSuccess(tx as ContractTransaction),
+                onError: (error: any) => handleError(error),
+              });
+            }}
+          >
+            GodMode
+          </button>
+        </div>
+      ) : (
+        <div></div>
+      )}
+
+      <FunnyImageModal isOpen={isOpen} closeModal={closeModal} />
+    </div>
+  );
+}
+
+export default GodModeButton;
