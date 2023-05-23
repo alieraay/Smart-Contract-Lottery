fc70f9cb85a4422aa77990249e9f2aacaec8ea4e
Ali Eray <alieraay@gmail.com>
2023-05-23 05:13:29 +0300

    deployed

--- backend/deploy/01-deploy-lottery.js
+++ backend/deploy/01-deploy-lottery.js
@@ -1,30 +1,28 @@
-const {getChainId} = require("hardhat")
-const {networkConfig, developmentChains} = require("../helper-hardhat-config")
-const {network, ethers} = require("hardhat")
-const {verify} = require("../utils/verify")
-require("dotenv").config();
+const { getChainId } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network, ethers } = require("hardhat")
+const { verify } = require("../utils/verify")
+require("dotenv").config()
 
 const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("1")
 
-module.exports = async ({getNamedAccounts, deployments}) => {
-    const {deploy, log} = deployments
-    const { deployer} = await getNamedAccounts()
-    const chainId =network.config.chainId
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
     let vrfCoordinatorV2Address
     let _subscriptionId
 
-    if(developmentChains.includes(network.name)){
+    if (developmentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
         const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
         const transactionReceipt = await transactionResponse.wait(1)
-        _subscriptionId = transactionReceipt.events[0].args.subId 
+        _subscriptionId = transactionReceipt.events[0].args.subId
         await vrfCoordinatorV2Mock.fundSubscription(_subscriptionId, VRF_SUB_FUND_AMOUNT)
-
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
         _subscriptionId = networkConfig[chainId]["subscriptionId"]
-
     }
 
     const entryPrice = networkConfig[chainId]["entryPrice"]
@@ -34,18 +32,27 @@ module.exports = async ({getNamedAccounts, deployments}) => {
     const numWords = networkConfig[chainId]["numWords"]
     const interval = networkConfig[chainId]["interval"]
 
-    const args = [vrfCoordinatorV2Address, entryPrice, keyHash, _subscriptionId, requestConfirmation, callbackGasLimit, numWords, interval ]
+    const args = [
+        vrfCoordinatorV2Address,
+        entryPrice,
+        keyHash,
+        _subscriptionId,
+        requestConfirmation,
+        callbackGasLimit,
+        numWords,
+        interval,
+    ]
     const lottery = await deploy("Lottery", {
         from: deployer,
         args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
-    console.log("----****---DEPLOYER: ",deployer)
+    console.log("----****---DEPLOYER: ", deployer)
 
-    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        await verify(lottery.address,args)
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(lottery.address, args)
         log("-------------------------------------------------")
     }
 }
-module.exports.tags = ["all","lottery"]
+module.exports.tags = ["all", "lottery"]
