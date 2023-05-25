15447a27c6ed1f412566329d166410ce1b47273d
Ali Eray <alieraay@gmail.com>
2023-05-25 04:58:23 +0300

    update

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,22 +1,14 @@
-// _app.tsx dosyası
-
 import "../styles/global.css"
-import type { AppProps } from "next/app"
-import { MoralisProvider, useMoralis } from "react-moralis"
-import React from "react"
+import type {AppProps} from "next/app"
+import {MoralisProvider} from "react-moralis"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const { isWeb3Enabled } = useMoralis()
-
-  if (!isWeb3Enabled) {
-    return <div>You are not connected to the network. Please connect to the Goerli network.</div>
-  }
-
-  return (
-    <MoralisProvider initializeOnMount={false}>
-      <Component {...pageProps} />
-    </MoralisProvider>
-  )
+function MyApp({ Component, pageProps} : AppProps) {
+    return (
+        <MoralisProvider initializeOnMount={false}>
+            
+            <Component {...pageProps} /> 
+        </MoralisProvider>
+    )
 }
 
 export default MyApp
