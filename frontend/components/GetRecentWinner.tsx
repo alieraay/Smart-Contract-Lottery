0246a2f3b32e8562c10831f5625a5230aa4537dc
Ali Eray <alieraay@gmail.com>
2023-05-26 16:38:29 +0300

    update

--- frontend/components/GetRecentWinner.tsx
+++ frontend/components/GetRecentWinner.tsx
@@ -6,13 +6,12 @@ function GetRecentWinner() {
     const {winnerTransactionHash} = useLotteryState()
 
     const winnerAddressLink = `https://goerli.etherscan.io/address/${recentWinner}`
-    const winnerTxLink = `https://goerli.etherscan.io/tx/${winnerTransactionHash}`
 
     console.log(winnerTransactionHash)
     return (
         <div className="text-white text-[20px] justify-center flex ">
         
-            <a className="hover:bg-red-300" href={winnerTxLink} target="_blank" rel="noopener noreferrer">Last Winner: {recentWinner}</a>
+            <a className="hover:bg-red-300" href={winnerAddressLink} target="_blank" rel="noopener noreferrer">Last Winner: {recentWinner}</a>
         </div>
     )
 }
